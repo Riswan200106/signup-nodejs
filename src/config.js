49c92fda4726310a6e6747a54c94e0305d5155ef
Riswan200106 <riswanrichu621@gmail.com>
2024-02-29 21:54:52 +0530

    first commit

---
+++ src/config.js
@@ -0,0 +1,33 @@
+
+const mongoose = require('mongoose');
+const connect = mongoose.connect(`mongodb+srv://riswanrichu621:Risvan123@cluster0.5ilg3dy.mongodb.net/login-form`)
+
+   
+// Check database connected or not
+connect.then(() => {
+    console.log("Database Connected Successfully");
+})
+.catch(() => {
+    console.log("Database cannot be Connected");
+})
+
+// Create Schema
+const Loginschema = new mongoose.Schema({
+    name: {
+        type:String,
+        required: true
+    },
+    email: {
+        type:String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+});
+
+// collection part
+const collection = new mongoose.model("users", Loginschema);
+
+module.exports = collection;
